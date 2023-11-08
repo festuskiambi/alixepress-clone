05ba76ef090f1f7a8fbe9db19844091655fac854
festuskiambi <kiamfestoh@gmail.com>
2023-11-08 10:30:35 +0300

    Add Shopping cart.
    Signed-off-by: festuskiambi <kiamfestoh@gmail.com>

--- pages/shoppingcart.vue
+++ pages/shoppingcart.vue
@@ -41,8 +41,51 @@
                                 @selectedRadio="selectedRadioFunc" />
                         </div>
                     </div>
-
                 </div>
+                <div class="md:hidden block my-4" />
+                <div class="md:w-[35%]">
+                    <div id="Summary" class="bg-white rounded-lg p-4">
+                        <div class="text-2xl font-extrabold mb-2">Summary</div>
+                        <div class="flex items-center justify-between my-4">
+                            <div class="font-semibold">Total</div>
+                            <div class="text-2xl font-semibold">
+                                $ <span class="font-extrabold">{{ totalPriceComputed }}</span>
+                            </div>
+                        </div>
+
+                        <button @click="goToCheckout" class="
+                                flex
+                                items-center
+                                justify-center
+                                bg-[#FD374F] 
+                                w-full 
+                                text-white 
+                                text-[21px] 
+                                font-semibold 
+                                p-1.5 
+                                rounded-full
+                                mt-4
+                            ">
+                            Checkout
+                        </button>
+                    </div>
+                    <div id="PaymentProtection" class="bg-white rounded-lg p-4 mt-4">
+                        <div class="text-lg font-semibold mb-2">Payment methods</div>
+                        <div class="flex items-center justify-start gap-8 my-4">
+                            <div v-for="card in cards">
+                                <img class="h-6" :src="card">
+                            </div>
+                        </div>
+                        <div class="border-b" />
+                        <div class="text-lg font-semibold mb-2 mt-2">Buyer Protection</div>
+                        <p class="my-2">
+                            Get full refund if the item is not as described or if is not delivered
+                        </p>
+                    </div>
+                </div>
+
+
+
 
             </div>
 
@@ -54,10 +97,63 @@
 
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+let selectedArray = ref([])
+
+onMounted(() => {
+    setTimeout(() => userStore.isLoading = false, 200)
+})
+
+const cards = ref([
+    'visa.png',
+    'mastercard.png',
+    'paypal.png',
+    'applepay.png',
+])
+
+const totalPriceComputed = computed(() => {
+    let price = 0
+    userStore.cart.forEach(prod => {
+        price += prod.price
+    })
+    return price / 100
+})
+
+const selectedRadioFunc = (e) => {
+
+    if (!selectedArray.value.length) {
+        selectedArray.value.push(e)
+        return
+    }
+
+    selectedArray.value.forEach((item, index) => {
+        if (e.id != item.id) {
+            selectedArray.value.push(e)
+        } else {
+            selectedArray.value.splice(index, 1);
+        }
+    })
+}
+
+const goToCheckout = () => {
+    let ids = []
+    userStore.checkout = []
+
+    selectedArray.value.forEach(item => ids.push(item.id))
+
+    let res = userStore.cart.filter((item) => {
+        return ids.indexOf(item.id) != -1
+    })
+
+    res.forEach(item => userStore.checkout.push(toRaw(item)))
+
+    return navigateTo('/checkout')
+}
 
 const products = [
-    {id: 1, title: "Title 1", description: "This is a description", url: "https://picsum.photos/id/7/800/800", price: 9999 },
-    {id: 2, title: "Title 1", description: "This is a description", url: "https://picsum.photos/id/6/800/800", price: 9999 },
-     
+    { id: 1, title: "Title 1", description: "This is a description", url: "https://picsum.photos/id/7/800/800", price: 9999 },
+    { id: 2, title: "Title 1", description: "This is a description", url: "https://picsum.photos/id/6/800/800", price: 9999 },
+
 ]
 </script>

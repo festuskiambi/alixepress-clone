6551ee5758527f6152434b7ea5a1199bda4647a0
festuskiambi <kiamfestoh@gmail.com>
2023-11-14 09:53:23 +0300

    Add logic to shopping cart
    Signed-off-by: festuskiambi <kiamfestoh@gmail.com>

--- pages/shoppingcart.vue
+++ pages/shoppingcart.vue
@@ -1,7 +1,7 @@
 <template>
     <MainLayout>
         <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            <div v-if="false" class="h-[500px] flex items-center justify-center">
+            <div v-if="!userStore.cart.length" class="h-[500px] flex items-center justify-center">
                 <div class="pt-20">
                     <img class="mx-auto" width="250" src="/cart-empty.png">
                     <div class="text-xl text-center mt-4">No items yet?</div>
@@ -27,7 +27,7 @@
                 <div class="md:w-[65%]">
                     <div class="bg-white rounded-lg p-4">
                         <div class="text-2xl font-bold mb-2">
-                            Shopping Cart (0)
+                            Shopping Cart ({{ userStore.cart.length }})
                         </div>
                     </div>
 
@@ -36,11 +36,11 @@
                     </div>
 
                     <div id="Items" class="bg-white rounded-lg p-4 mt-4">
-                        <div v-for="product in products">
+                        <div v-for="product in userStore.cart">
                             <CartItem :product="product" :selectedArray="selectedArray"
                                 @selectedRadio="selectedRadioFunc" />
                         </div>
-                    </div>
+                    </div> 
                 </div>
                 <div class="md:hidden block my-4" />
                 <div class="md:w-[35%]">
@@ -151,9 +151,5 @@ const goToCheckout = () => {
     return navigateTo('/checkout')
 }
 
-const products = [
-    { id: 1, title: "Title 1", description: "This is a description", url: "https://picsum.photos/id/7/800/800", price: 9999 },
-    { id: 2, title: "Title 1", description: "This is a description", url: "https://picsum.photos/id/6/800/800", price: 9999 },
 
-]
 </script>
